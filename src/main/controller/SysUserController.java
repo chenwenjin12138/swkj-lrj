06caf0fd58b93f87c5acb9dccac154b9ea24df44
fuling <fulingdyx@163.com>
2020-06-02 14:32:57 +0800

    修改登录

--- src/main/controller/SysUserController.java
+++ src/main/controller/SysUserController.java
@@ -1,27 +1,21 @@
 package controller;
 
-import com.fasterxml.jackson.databind.util.JSONPObject;
-import com.sun.deploy.net.HttpResponse;
+import dto.ReturnData;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import net.sf.json.JSONObject;
 import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.authc.*;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import pojo.SysUser;
 
 /**
  * @author : cwj
@@ -43,26 +37,20 @@ public class SysUserController{
 
     /**
      * 后台管理系统登录：用户名+密码登录
-     * @param userName
-     * @param passWord
-     * @param request
      * @return
      */
     @ApiOperation("用户登录")
     @RequestMapping(value = "/userLogin",method = {RequestMethod.GET,RequestMethod.POST})
     @ResponseBody
-    public Map<String,Object> userLogin(String userName, String passWord, HttpServletRequest request, HttpServletResponse response){
-        Map<String, Object> resultMap = new HashMap<String, Object>();
+    public ReturnData<Boolean> userLogin(@RequestBody SysUser sysUser){
+        String userName = sysUser.getAdminName();
+        String passWord = sysUser.getAdminPassword();
         try {
             if(StringUtils.isEmpty(userName) || StringUtils.isEmpty(passWord)){
-                resultMap.put("errorCode", 1);
-                resultMap.put("errorTip", "参数有误,请检查参数");
-                return resultMap;
+                return new ReturnData<Boolean>(ReturnData.Fail_CODE,"参数有误,请检查参数",false);
             }
         }catch (Exception e){
-            resultMap.put("errorCode", 2);
-            resultMap.put("errorTip","系统异常，登录失败");
-            return resultMap;
+            return new ReturnData<Boolean>(ReturnData.Fail_CODE,"系统异常，登录失败",false);
         }
         // 从SecurityUtils里边创建一个 subject
         Subject subject = SecurityUtils.getSubject();
@@ -72,32 +60,21 @@ public class SysUserController{
         try {
             subject.login(token);
         } catch (UnknownAccountException uae) {
-            resultMap.put("errorCode", 3);
-            resultMap.put("errorTip","未知账户");
-            return resultMap;
+            return new ReturnData<Boolean>(ReturnData.Fail_CODE,"未知账户",false);
         } catch (IncorrectCredentialsException ice) {
-            resultMap.put("errorCode", 4);
-            resultMap.put("errorTip","密码错误");
-            return resultMap;
-        } /*catch (LockedAccountException lae) {
-            return "账户已锁定";
-        } catch (ExcessiveAttemptsException eae) {
-            return "用户名或密码错误次数过多";
-        } */catch (AuthenticationException ae) {
-            resultMap.put("errorCode", 5);
-            resultMap.put("errorTip","用户名或密码不正确！");
-            return resultMap;
+            return new ReturnData<Boolean>(ReturnData.Fail_CODE,"密码错误",false);
+
+        }catch (AuthenticationException ae) {
+            return new ReturnData<Boolean>(ReturnData.Fail_CODE,"用户名或密码不正确",false);
+
         }
         if (subject.isAuthenticated()) {
             System.out.println("认证成功了！。。。。。。。");
-            resultMap.put("errorCode", 0);
-            resultMap.put("errorTip","main");
+            return new ReturnData<Boolean>(ReturnData.SUCCESS_CODE,"登录成功",true);
         } else {
             token.clear();
-            resultMap.put("errorCode", 6);
-            resultMap.put("errorTip","登录失败,请联系客服人员！");
+            return new ReturnData<Boolean>(ReturnData.SUCCESS_CODE,"登录失败,请联系客服人员",true);
         }
-        return resultMap;
     }
 
 }
